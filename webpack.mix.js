44d5a8e351833f13c71a0f10becacd30ba3900db
Matt Jones <matt@azmatt.co.uk>
2021-06-24 11:33:20 +0100

    Latest npm updates

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,8 @@
-let mix = require('laravel-mix');
-let StyleLintPlugin = require('stylelint-webpack-plugin');
+let mix = require('laravel-mix')
+let StyleLintPlugin = require('stylelint-webpack-plugin')
+let path = require('path')
+
+console.log(path.resolve('./resources/js'));
 
 /*
  |--------------------------------------------------------------------------
@@ -12,13 +15,16 @@ let StyleLintPlugin = require('stylelint-webpack-plugin');
  |
  */
 
+mix.alias({
+    '@': path.join(__dirname, 'resources/js'),
+    vue$: path.join(__dirname, 'node_modules/vue/dist/vue.runtime.esm.js'),
+});
+
+mix.vue();
+
 mix.webpackConfig({
     resolve: {
         extensions: ['js', 'vue'],
-        alias: {
-            'vue$': 'vue/dist/vue.runtime.esm.js',
-            '@': path.resolve('./resources/js')
-        },
     },
     plugins: [
         new StyleLintPlugin({
@@ -26,12 +32,12 @@ mix.webpackConfig({
             configFile: './.stylelintrc'
         }),
     ]
-});
+})
 
-var outName = 'dev';
+var outName = 'dev'
 
 if (process.env.NODE_ENV == 'production') {
-    outName = 'prod';
+    outName = 'prod'
 }
 
 mix.js('resources/js/app.js', 'public/js/app.' + outName + '.js')
@@ -43,11 +49,11 @@ mix.js('resources/js/app.js', 'public/js/app.' + outName + '.js')
         'vue',
         'vuex',
         'vue-router',
-    ], 'public/js/vendor.' + outName + '.js');
+    ], 'public/js/vendor.' + outName + '.js')
 
-mix.sass('resources/sass/app.scss', 'public/css/app.' + outName + '.css');
+mix.sass('resources/sass/app.scss', 'public/css/app.' + outName + '.css')
 
 
 if (mix.inProduction()) {
-    mix.version();
+    mix.version()
 }
